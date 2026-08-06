a71fa4914afdc86d6f0cffcdfb455973015ab29e
nkurunzizamuganza94-lgtm <nkurunzizamuganza94@gmail.com>
2026-08-06 15:35:08 +0200

    Update logout.php

--- clinique_veterinaire/logout.php
+++ clinique_veterinaire/logout.php
@@ -1,6 +1,21 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 session_destroy();
+
 header('Location: login.php');
+echo "<script>window.location.href='login.php';</script>";
 exit;
 ?>
